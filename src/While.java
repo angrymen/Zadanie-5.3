65d75db60212d3ec188981d910bb2e6274bdd9a1
Michal Madejski <michalmadejski00@gmail.com>
2018-09-23 11:06:46 +0200

    Commit 1 Zadanie5.3

---
+++ src/While.java
@@ -0,0 +1,18 @@
+public class While {
+
+    public static void main(String[] args) {
+
+        double y = 3.1;
+        double z = 0;
+
+        while (z <= y) {
+
+                System.out.printf("%.1f\n", z);
+
+            z = z + 0.1;
+
+        }
+
+    }
+
+}
